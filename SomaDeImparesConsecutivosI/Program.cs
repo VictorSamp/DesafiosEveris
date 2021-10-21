21e793303ff6abb2fc21e4dbbbb3239e1ef20182
Victor de Magalhães Sampaio <victor.msampaio@outlook.com.br>
2021-10-21 19:37:26 -0300

    feat: adiciona desafio SomaDeImparesConsecutivosI

---
+++ SomaDeImparesConsecutivosI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SomaDeImparesConsecutivosI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int entrada1, entrada2, minimo, maximo, soma;
+
+            entrada1 = int.Parse(Console.ReadLine());
+            entrada2 = int.Parse(Console.ReadLine());
+
+            if (entrada1 < entrada2)
+            {
+                minimo = entrada1;
+                maximo = entrada2;
+            }
+            else
+            {
+                minimo = entrada2;
+                maximo = entrada1;
+            }
+
+            soma = 0;
+            for (int i = minimo + 1; i < maximo; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma = i;     //complete o código nos espaços em branco
+                }
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
