44bf47a5594ad6449c77e2afaed835b7b1466df0
Victor de Magalhães Sampaio <victor.msampaio@outlook.com.br>
2021-10-21 20:08:54 -0300

    feat: adiciona desafio PUM

---
+++ PUM/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PUM
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int entrada = int.Parse(Console.ReadLine());
+
+            int contador = 1;
+
+            for (int i = 0; i < entrada; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (contador % 4 == 0)
+                    {
+                        Console.WriteLine("PUM");
+                    }
+                    else
+                    {
+                        Console.Write($"{contador} ");
+                    }
+
+                    contador++;
+                }
+            }
+        }
+    }
+}
