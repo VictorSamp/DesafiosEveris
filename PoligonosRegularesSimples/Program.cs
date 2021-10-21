926efed15926e9ab116d7f673e8626d9c55701a8
Victor de Magalhães Sampaio <victor.msampaio@outlook.com.br>
2021-10-21 20:19:22 -0300

    feat: adiciona desafio PoligonosRegularesSimples

---
+++ PoligonosRegularesSimples/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PoligonosRegularesSimples
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string[] entrada = Console.ReadLine().Split(' ');
+
+            int quantidadeDeLados = int.Parse(entrada[0]);
+            int comprimentoDoLado = int.Parse(entrada[1]);
+
+            int perimetro = quantidadeDeLados * comprimentoDoLado;
+
+            Console.WriteLine(perimetro);
+        }
+    }
+}
