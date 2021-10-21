b3163b9da4a3f62b3b4c9ef2d5ec287922fc4379
Victor de Magalhães Sampaio <victor.msampaio@outlook.com.br>
2021-10-20 21:27:21 -0300

    feat:adiciona desafio Quadrante

---
+++ Quadrante/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Quadrante
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int pontoX, pontoY;
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                pontoX = int.Parse(input[0]);
+                pontoY = int.Parse(input[1]);
+
+                if (pontoX == 0 || pontoY == 0)
+                    break;
+                else if (pontoX > 0 && pontoY > 0)
+                    Console.WriteLine("primeiro");
+                else if (pontoX < 0 && pontoY > 0)
+                    Console.WriteLine("segundo");
+                else if (pontoX < 0 && pontoY < 0)
+                    Console.WriteLine("terceiro");
+                else
+                    Console.WriteLine("quarto");
+            }
+        }
+    }
+}
