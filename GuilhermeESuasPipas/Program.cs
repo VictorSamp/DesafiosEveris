eb490174717744e3e0b517725bbfccaf45fbb870
Victor de Magalhães Sampaio <victor.msampaio@outlook.com.br>
2021-10-21 19:32:17 -0300

    feat: adiciona desafio GuilhermeESuasPipas

---
+++ GuilhermeESuasPipas/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GuilhermeESuasPipas
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int entrada = int.Parse(Console.ReadLine());
+            int resposta = (entrada * (entrada - 3)) / 2;
+            Console.WriteLine(resposta);
+        }
+    }
+}
