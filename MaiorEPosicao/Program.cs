a93d8d062ff54515523bbf24d55f385d90ccca02
Victor de Magalhães Sampaio <victor.msampaio@outlook.com.br>
2021-10-21 19:19:46 -0300

    feat: adiciona desafio MaiorEPosicao

---
+++ MaiorEPosicao/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MaiorEPosicao
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int entrada;
+            int maior = 0;
+            int posicao = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                entrada = Convert.ToInt32(Console.ReadLine());
+                if (entrada > maior)
+                {
+                    maior = entrada;
+                    posicao = i;
+                }
+            }
+            Console.WriteLine(maior);
+            Console.WriteLine(posicao);
+        }
+    }
+}
