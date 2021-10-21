7bf078e46cc15aaca4d34c46461e967a87db336b
Victor de Magalhães Sampaio <victor.msampaio@outlook.com.br>
2021-10-20 21:08:42 -0300

    Add project files.

---
+++ HoHoHo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HoHoHo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int quantidadeDeHo = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quantidadeDeHo; i++)
+            {
+                if (i < quantidadeDeHo - 1)
+                {
+                    Console.Write("Ho ");
+                }
+                else
+                {
+                    Console.Write("Ho!");
+                }
+            }
+        }
+    }
+}
