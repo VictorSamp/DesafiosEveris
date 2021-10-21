a43cefe3fad47c91671d1b2b1777d0e74d6ed516
Victor de Magalhães Sampaio <victor.msampaio@outlook.com.br>
2021-10-20 21:14:29 -0300

    feat: adiciona desafio Pneu

---
+++ Pneu/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Pneu
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int pressaoDesejada = int.Parse(Console.ReadLine());
+            int pressaoAtual = int.Parse(Console.ReadLine());
+
+            int diferencaPressao = pressaoDesejada - pressaoAtual;
+
+            Console.WriteLine(diferencaPressao);
+        }
+    }
+}
